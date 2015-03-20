6936a9ccf6fe1ad9064a27fe4201a8e6a74813ca
Cauê Waneck <waneck@gmail.com>
2015-03-20 16:53:57 -0300

    Simplify HttpRequest

--- src/mweb/tools/HttpRequest.hx
+++ src/mweb/tools/HttpRequest.hx
@@ -1,26 +1,16 @@
 package mweb.tools;
-#if neko
-import neko.Web;
-#elseif php
-import php.Web;
-#elseif (croxit_1)
-import croxit.Web;
-#end
 
-@:forward abstract HttpRequest(IHttpRequestData) from IHttpRequestData
+@:forward abstract HttpRequest(HttpRequestData) from HttpRequestData
 {
 	@:extern inline public function new(data)
 	{
 		this = data;
 	}
 
-#if (neko || php || croxit_1)
-	@:from inline public static function fromWeb(cls:Class<Web>):HttpRequest
+	@:from inline public static function fromWeb(cls:LikeWeb):HttpRequest
 	{
-		// sadly we need this because of DCE
-		return new NekoWebRequest();
+		return new WebRequest(cls);
 	}
-#end
 
 	public static function fromData(method:String, uri:String, ?params:Map<String,Array<String>>):HttpRequest
 	{
@@ -53,27 +43,27 @@ import croxit.Web;
 	}
 }
 
-interface IHttpRequestData
+class HttpRequestData
 {
 	/**
 		Should return the method (verb) used by the request - values like GET/POST
 	 **/
-	public function getMethod():String;
+	public function getMethod():String { throw "Not Implemented"; }
 
 	/**
 		Should return the URI queried by the HTTP request
 	 **/
-	public function getUri():String;
+	public function getUri():String { throw "Not Implemented"; }
 
 	/**
 		Should return a String containing the parameters.
 		It is advised that as a security measure on a non-GET request, only the parameters passed
 		through the body of the message are sent here.
 	 **/
-	public function getParamsData():Map<String,Array<String>>;
+	public function getParamsData():Map<String,Array<String>> { throw "Not Implemented"; }
 }
 
-class HttpRequestStatic implements IHttpRequestData
+class HttpRequestStatic extends HttpRequestData
 {
 	var method:String;
 	var uri:String;
@@ -89,7 +79,7 @@ class HttpRequestStatic implements IHttpRequestData
 	/**
 		Should return the method (verb) used by the request - values like GET/POST
 	 **/
-	public function getMethod():String
+	override public function getMethod():String
 	{
 		return method;
 	}
@@ -97,7 +87,7 @@ class HttpRequestStatic implements IHttpRequestData
 	/**
 		Should return the URI queried by the HTTP request
 	 **/
-	public function getUri():String
+	override public function getUri():String
 	{
 		return uri;
 	}
@@ -107,43 +97,50 @@ class HttpRequestStatic implements IHttpRequestData
 		It is advised that as a security measure on a non-GET request, only the parameters passed
 		through the body of the message are sent here.
 	 **/
-	public function getParamsData():Map<String,Array<String>>
+	override public function getParamsData():Map<String,Array<String>>
 	{
 		return params;
 	}
 }
 
-#if (neko || php || croxit_1)
-private class NekoWebRequest implements IHttpRequestData
+typedef LikeWeb = {
+	function getMethod():String;
+	function getURI():String;
+	function getParamsString():String;
+	function getPostData():String;
+}
+
+private class WebRequest extends HttpRequestData
 {
-	public function new()
+	var web:LikeWeb;
+	public function new(web)
 	{
+		this.web = web;
 	}
 
-	public function getMethod():String
+	override public function getMethod():String
 	{
-		return Web.getMethod();
+		return web.getMethod();
 	}
 
-	public function getUri():String
+	override public function getUri():String
 	{
-		return Web.getURI();
+		return web.getURI();
 	}
 
-	public function getParamsData():Map<String,Array<String>>
+	override public function getParamsData():Map<String,Array<String>>
 	{
-		var verb = Web.getMethod();
+		var verb = web.getMethod();
 		var args = new Map();
 
 		switch(verb)
 		{
 			case "get":
-				HttpRequest.splitArgs( StringTools.replace(Web.getParamsString(), ';', '&'), args );
+				HttpRequest.splitArgs( StringTools.replace(web.getParamsString(), ';', '&'), args );
 			case _:
-				HttpRequest.splitArgs( Web.getPostData(), args );
+				HttpRequest.splitArgs( web.getPostData(), args );
 		}
 
 		return args;
 	}
 }
-#end
